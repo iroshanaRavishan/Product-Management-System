410607e9ae17b9b613667ed328e6008a92a581ed
IroshanaRavishan <bandarairoshana@gamil.com>
2024-05-15 23:27:56 +0530

    Revert "added Bootstrap, routing and prod component"
    This reverts commit ad55395ee1ca188b3278fb570b07f1d887e9ac49.

--- PMS.UI/src/app/app-routing.module.ts
+++ PMS.UI/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [
-  {path: '', component: ProductsComponent},
-  {path: 'products', component: ProductsComponent}
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PMS.UI/src/app/models/Product.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-    id: string,
-    name: string,
-    type: string,
-    color: string,
-    price: string,
-}
