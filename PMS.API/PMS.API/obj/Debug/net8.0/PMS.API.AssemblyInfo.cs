44c86f970f6eaee6997b7fc7ff4ed1c932d601fb
IroshanaRavishan <bandarairoshana@gamil.com>
2024-08-01 16:24:03 +0530

    .net constants file changes (auto changed)

--- PMS.API/PMS.API/obj/Debug/net8.0/PMS.API.AssemblyInfo.cs
+++ PMS.API/PMS.API/obj/Debug/net8.0/PMS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd45cbab8bf368100e4f2cd41763f60ec6ebbef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecf7fcf67016c7fc066a430099b56e2805136f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
