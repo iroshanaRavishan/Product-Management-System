b9062afad626006e7c694618dd3f62cf57a5d1de
IroshanaRavishan <bandarairoshana@gamil.com>
2024-08-02 23:38:51 +0530

    added Auth Component

--- PMS.UI/src/app/app-routing.module.ts
+++ PMS.UI/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 const routes: Routes = [
-  { path: '', component: ProductsComponent },
+  { path: '', component: AuthComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'product/add', component: AddProductComponent },
   { path: 'edit-product/:id', component: AddProductComponent }
